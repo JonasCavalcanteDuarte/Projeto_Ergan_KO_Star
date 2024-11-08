a207ad9dee8b211d1b96f4ea8ca0d40b04f8f824
Jonas Cavalcante Duarte <jonas-cavalcante16@outlook.com>
2024-11-08 09:29:10 -0300

    Update lang all pages to pt-BR

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include('./php/protectML.php');
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- php/editarUsuario.php
+++ php/editarUsuario.php
@@ -8,7 +8,7 @@ $row = $sql_exec->fetch_object();
 
 ?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
